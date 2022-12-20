7e82340355ea9b3644524cdcf9f5fa98078d7793
Darsh <darshankondhare2698@gmail.com>
2022-12-20 19:43:58 +0530

    [darsh] Add: state Code Analyzer

---
+++ IndianStateCensusAnalyzerProblemStatement/CSVStateCode.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyzerProblemStatement
+{
+    public class CSVStateCode
+    {
+        public int ReadStateCodeData(string filePath1)
+        {
+            using (var reader = new StreamReader(filePath1))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<StateCodeDAO>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data);
+                }
+                return records.Count() - 1;
+            }
+        }
+    }
+}
--- IndianStateCensusAnalyzerProblemTest/UnitTest1.cs
+++ IndianStateCensusAnalyzerProblemTest/UnitTest1.cs
@@ -8,6 +8,11 @@ namespace IndianStateCensusAnalyzerProblemTest
         public string stateCensusIncorrectFilePath = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\";
         public string typeIncorrectFilePath = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\Demo.txt";
         public string delimeterFilePath = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\CSVStateCensusDelimeter.csv";
+        public string stateCodeFilePath = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\StateCode.csv";
+        public string stateCodeIncorrectFilePath = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\";
+        public string typeIncorrectFilePath1 = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\Demo.txt";
+        public string delimeterFilePath1 = @"C:\GetRepositry\indianStateCensusAnalyzerProblem\IndianStateCensusAnalyzerProblemStatement\Files\CSVStateCodeDelimeter.csv";
+
 
         [Test]
         public void GivenStateCensusData_WhenAnalyzed_ShouldReturnNoOfRecordsMatches()
@@ -68,6 +73,69 @@ namespace IndianStateCensusAnalyzerProblemTest
                 Assert.IsTrue(records);
             }
             catch (StateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect Header");
+            }
+        }
+        [Test]
+        public void GivenStateCodeData_WhenAnalyzed_ShouldReturnNoOfRecordsMatches()
+        {
+            IndianCensusAnalyzer analyzerProblem = new IndianCensusAnalyzer();
+            CSVStateCode cSVState = new CSVStateCode();
+            Assert.AreEqual(cSVState.ReadStateCodeData(stateCodeFilePath), analyzerProblem.ReadStateCodeData(stateCodeFilePath));
+        }
+        [Test]
+        public void GivenStateCodeDataFileIncorrect_WhenAnalyzed_ShouldReturnException()
+        {
+            IndianCensusAnalyzer analyzer = new IndianCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCodeData(stateCodeIncorrectFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect file path");
+            }
+
+        }
+        [Test]
+        public void GivenStateCodeDataFileTypeIncorrect_WhenAnalyzed_ShouldReturnException()
+        {
+            IndianCensusAnalyzer analyzer = new IndianCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCodeData(typeIncorrectFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "Type Incorrect");
+            }
+        }
+
+        [Test]
+        public void GivenStateCodeDataFileDelimeterIncorrect_WhenAnalyzed_ShouldReturnException()
+        {
+            IndianCensusAnalyzer analyzer = new IndianCensusAnalyzer();
+            try
+            {
+                int records = analyzer.ReadStateCodeData(delimeterFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect Delimeter");
+            }
+        }
+
+        [Test]
+        public void GivenStateCodeDataFileHeaderIncorrect_WhenAnalyzed_ShouldReturnException()
+        {
+            IndianCensusAnalyzer indianCencusAnalyzer = new IndianCensusAnalyzer();
+            try
+            {
+                bool records = indianCencusAnalyzer.ReadStateCodeData(delimeterFilePath, "SrNo, State, TIN, StateCode");
+                Assert.IsTrue(records);
+            }
+            catch (StateCodeException ex)
             {
                 Assert.AreEqual(ex.Message, "Incorrect Header");
             }
